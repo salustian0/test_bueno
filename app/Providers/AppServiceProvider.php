47c69c6ac010525aa912faa9365ec42249dcb7b2
Renan Salustiano <renan.silva@kron.digital>
2023-11-30 16:26:34 -0300

    Feat: binds interface to class

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,15 @@
 
 namespace App\Providers;
 
+
+use App\Repositories\Interfaces\IRoleRepository;
+use App\Repositories\Interfaces\IUserRepository;
+use App\Repositories\RoleRepository;
+use App\Repositories\UserRepository;
+use App\Services\Interfaces\IRoleService;
+use App\Services\Interfaces\IUserService;
+use App\Services\RoleService;
+use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,8 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
-    }
+        $this->app->bind(IRoleService::class, RoleService::class);
+        $this->app->bind(IRoleRepository::class, RoleRepository::class);
+
+        $this->app->bind(IUserService::class, UserService::class);
+        $this->app->bind(IUserRepository::class, UserRepository::class );    }
 
     /**
      * Bootstrap any application services.
